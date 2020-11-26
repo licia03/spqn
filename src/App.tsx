4f7f58d316af032b0b3f42cd140c8ece05d1b55e
licia03 <lgavina7@gmail.com>
2020-11-26 22:48:46 +0100

    manage change numeral system

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback } from "react";
 
-import NumberField from './components/NumberField/NumberField';
-import './App.css';
-import KeyboardRoman from './components/KeyboardRoman/KeyboardRoman';
-import KeyboardArabic from './components/KeyboardArabic/KeyboardArabic';
-import Switch from './components/Switch/Switch';
+import NumberField from "./components/NumberField/NumberField";
+import "./App.css";
+import KeyboardRoman from "./components/KeyboardRoman/KeyboardRoman";
+import KeyboardArabic from "./components/KeyboardArabic/KeyboardArabic";
+import Switch from "./components/Switch/Switch";
+import { RomanValue, ArabicValue } from "./components/Key/Key";
 
 export type NumeralSystem = "arabic" | "roman";
 
 function App() {
-  const [selectedNumeralSystem, setSelectedNumeralSystem] = useState<NumeralSystem>('roman');
+  const [selectedNumeralSystem, setSelectedNumeralSystem] = useState<
+    NumeralSystem
+  >("roman");
+  const [number, setNumber] = useState<RomanValue | ArabicValue | string>("");
 
-  const handleKeyPressed = useCallback(() => {
+  const handleChangeNumeralSystem = useCallback((numeralSystem: NumeralSystem) => {
+    setNumber('');
+    setSelectedNumeralSystem(numeralSystem);
+  }, []);
 
+  const handleKeyPressed = useCallback((value: RomanValue | ArabicValue) => {
+    setNumber((prevState) => prevState.concat(value));
   }, []);
 
   return (
     <div className="App">
-      <NumberField label="Arabic" value='3' />
-      <NumberField label="Roman" value='iii' />
-      <Switch numeralSystem={selectedNumeralSystem} onChange={setSelectedNumeralSystem} />
-      {selectedNumeralSystem === 'roman' && <KeyboardRoman onKeyPressed={handleKeyPressed}/>}
-      {selectedNumeralSystem === 'arabic' && <KeyboardArabic onKeyPressed={handleKeyPressed}/>}
+      <NumberField
+        label="Arabic"
+        value={selectedNumeralSystem === "arabic" ? number : "11"}
+      />
+      <NumberField
+        label="Roman"
+        value={selectedNumeralSystem === "roman" ? number : "II"}
+      />
+      <Switch
+        numeralSystem={selectedNumeralSystem}
+        onChange={handleChangeNumeralSystem}
+      />
+      {selectedNumeralSystem === "roman" && (
+        <KeyboardRoman onKeyPressed={handleKeyPressed} />
+      )}
+      {selectedNumeralSystem === "arabic" && (
+        <KeyboardArabic onKeyPressed={handleKeyPressed} />
+      )}
     </div>
   );
 }
