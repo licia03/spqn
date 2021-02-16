5d681081cfe8c293088e4b99482bbfe110366c99
licia03 <lgavina7@gmail.com>
2021-02-16 22:30:54 +0100

    Update style
    Co-authored-by: Luca Gandini <luca.gandini88@gmail.com>

--- src/components/KeyboardRoman/KeyboardRoman.tsx
+++ src/components/KeyboardRoman/KeyboardRoman.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Keyboard from "../Keyboard/Keyboard";
-import Key  from "../Key/Key";
+import Key from "../Key/Key";
 import { ValidArabicNumber, RomanChar } from "../../types";
 
 interface Props {
-    onKeyPressed: (value: RomanChar | ValidArabicNumber) => void;
+  onKeyPressed: (value: RomanChar | ValidArabicNumber) => void;
 }
 
-const KeyboardRoman: React.FC<Props> = ({onKeyPressed}) => (
-  <Keyboard>
-    <Key value="I" onClick={onKeyPressed} />
-    <Key value="V" onClick={onKeyPressed} />
-    <Key value="X" onClick={onKeyPressed} />
-    <Key value="L" onClick={onKeyPressed} />
-    <Key value="C" onClick={onKeyPressed} />
-    <Key value="D" onClick={onKeyPressed} />
-    <Key value="M" onClick={onKeyPressed} />
-  </Keyboard>
+const KeyboardRoman: React.FC<Props> = ({ onKeyPressed }) => (
+  <>
+    <p>Enter a roman number between I and MMMCMXCIX</p>
+    <Keyboard>
+      <Key value="I" onClick={onKeyPressed} />
+      <Key value="V" onClick={onKeyPressed} />
+      <Key value="X" onClick={onKeyPressed} />
+      <Key value="L" onClick={onKeyPressed} />
+      <Key value="C" onClick={onKeyPressed} />
+      <Key value="D" onClick={onKeyPressed} />
+      <Key value="M" onClick={onKeyPressed} />
+    </Keyboard>
+  </>
 );
 
 export default KeyboardRoman;
